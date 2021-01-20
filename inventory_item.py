a8de9b81114f0ef41434a6723a3d052d2c306355
Yura Morozov <yura_morozov01@mail.ru>
2021-01-20 17:17:16 +0300

    feat: add steam auth

--- inventory_item.py
+++
@@ -1,9 +0,0 @@
-class InventoryItem:
-	def __init__(self, data):
-		self.data = data
-
-	def calc_total_price(self):
-		price = self.data.get('price')
-		amount = self.data.get('amount')
-		if price and amount:
-			self.data['total_price'] = float('{:.2f}'.format(float(price) * amount))
